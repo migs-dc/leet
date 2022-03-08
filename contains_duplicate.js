f5468982bcb4a4bea09487383ae320c4bfcf01a1
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-03-07 21:48:47 -0500

    using sort and then using a hashmap to get a much better solution

--- contains_duplicate.js
+++ contains_duplicate.js
@@ -11,36 +11,51 @@
 // a faster method would be to sort the array first
 // time: o(n log n)
 
+
+
 // an even faster on would be using a hash map
 // by sacrificing a little bit of space to improve time
 // time: o(n)
 // space: o(n)
 
+//  var containsDuplicate = function(nums) {
 
+    let hash = new Map();
+    
+    for (let i = 0; i < nums.length; i++){
+        if (hash.has(nums[i])){
+            return true;
+        }
+        hash.set(nums[i]);
+    }
+    
+    return false;
+    
+};
 
-// var containsDuplicate = function(nums) {
+var containsDuplicate = function(nums) {
     
-//     for(let i = 0; i < nums.length; i++){
-//         for(let j = 0; j < i; j++){
-//             if (nums[i] == nums[j]){
-//                 return true;
-//             }
-//         }
-//     }
+    for(let i = 0; i < nums.length; i++){
+        for(let j = 0; j < i; j++){
+            if (nums[i] == nums[j]){
+                return true;
+            }
+        }
+    }
 
-//     return false;
-// };
+    return false;
+};
 
-// var containsDuplicate = function(nums) {
+var containsDuplicate = function(nums) {
     
-//     let arr = [];
+    let arr = [];
     
-//     for(let i = 0; i < nums.length; i ++){
-//         if (arr.includes(nums[i])){
-//             return true;
-//         }
-//         arr.push(nums[i]);
-//     }
+    for(let i = 0; i < nums.length; i ++){
+        if (arr.includes(nums[i])){
+            return true;
+        }
+        arr.push(nums[i]);
+    }
     
-//     return false;
-// };
+    return false;
+};
