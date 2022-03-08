14c18ec0a28a45979d6638efd3deea0b0b3a2de0
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-03-07 21:25:31 -0500

    solution got a little bit better, but still pretty slow

--- contains_duplicate_II.js
+++ contains_duplicate_II.js
@@ -3,16 +3,29 @@
  * @return {boolean}
  */
 
-var containsDuplicate = function(nums) {
+ var containsDuplicate = function(nums) {
     
-    let arr = [];
-    
-    for(let i = 0; i < nums.length; i ++){
-        if (arr.includes(nums[i])){
-            return true;
-        }
-        arr.push(nums[i]);
-    }
-    
-    return false;
+  for(let i = 0; i < nums.length; i++){
+      for(let j = 0; j < i; j++){
+          if (nums[i] == nums[j]){
+              return true;
+          }
+      }
+  }
+  
+  return false;
 };
+
+// var containsDuplicate = function(nums) {
+    
+//     let arr = [];
+    
+//     for(let i = 0; i < nums.length; i ++){
+//         if (arr.includes(nums[i])){
+//             return true;
+//         }
+//         arr.push(nums[i]);
+//     }
+    
+//     return false;
+// };
