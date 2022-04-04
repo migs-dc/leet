45575bf3e16e20d0f2c4507644e74c3bc974f10f
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-04 13:04:22 -0400

    still some wrong test cases

--- validate_subsequence.js
+++ validate_subsequence.js
@@ -1,13 +1,13 @@
 function isValidSubsequence(array, sequence) {
-    if (array.length === 1 || array === array) return true;
+    if (array.length === 1 || array === sequence) return true;
       
       if (array.length < sequence.length) return false;
       
       let count = 0;
       
-      for (let x=0; x<array.length; x++){
-          if ( sequence.includes(array(x)) ){
-              if ( sequence.indexOf(array(x)) === count ){
+      for (let x = 0; x < array.length; x++){
+          if ( sequence.includes(array[x]) ){
+              if ( sequence.indexOf(array[x]) === count ){
                   count++;
               } else {
                   return false;
