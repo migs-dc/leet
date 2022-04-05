66667a8456910a7bfbabda0e1e833932cb2b7a3b
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-04 23:59:57 -0400

    roman_to_integer accepted solution

--- roman_to_integer.js
+++ roman_to_integer.js
@@ -2,7 +2,26 @@
  * @param {string} s
  * @return {number}
  */
- var romanToInt = function(s) {
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var romanToInt = function(s) {
+    const map = { I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000 };
+    var total = 0;
+    
+    for (let i=0; i<s.length; i++){        
+        if (map[s[i]] < map[s[i+1]]) total -= map[s[i]];
+        else total += map[s[i]];
+    }
+    
+    return total;
+};
+
+// wrong brute force
+
+var romanToInt = function(s) {
     let total = 0;
     
     for (let x = 0; x < s.length; x++){
