37f4584e35d3f11d2df9a5f636bd1922aa44d599
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-05 21:35:32 -0400

    longest common prefix, initial solution

---
+++ longest_common_prefix.js
@@ -0,0 +1,12 @@
+var longestCommonPrefix = function(strs) {
+    res = "";
+    
+    for (let i=0; i<strs[0].length; i++){
+        for (let j=1; j<strs.length; j++){
+            if ( strs[j][i] !== strs[0][i]) return res;
+        }
+        res += strs[0][i];
+    }
+    
+    return res;
+};
