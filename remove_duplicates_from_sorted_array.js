88191d5bf02c50c970ae055713eac69181c538b4
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-06 23:34:14 -0400

    remove duplicates from sorted array, psuedo code

--- remove_duplicates_from_sorted_array.js
+++ remove_duplicates_from_sorted_array.js
@@ -5,3 +5,14 @@
 var removeDuplicates = function(nums) {
     
 };
+
+// feels really wasteful
+// iterate through array
+// if i === i+1
+    // make i+1 = ""
+    // swap i+1 with the next elem until it reaches the end
+// if i != 1
+    // k++
+
+
+// return k
