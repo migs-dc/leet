3f4fe11abaab8532bc89ec796d817ef7161275a4
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-08 23:43:16 -0400

    remove duplicates from sorted array - comments

--- remove_duplicates_from_sorted_array.js
+++ remove_duplicates_from_sorted_array.js
@@ -3,19 +3,22 @@
  * @return {number}
 */
 
- var removeDuplicates = function(nums) {
-    if (nums.length === 0) return 0;
+// try reframing the question because this problem actually just asks for a unique element array
+// within nums so whatever comes after doesn't matter
+
+ var removeDuplicates = function(nums) { 
+    if (nums.length === 0) return 0; // return 0 if array is empty
     
-    let p1 = 0;
+    let p1 = 0; // pointer 1 on first element
     
-    for (let p2 = 1; p2 < nums.length; p2++){
-        if (nums[p1] !== nums[p2]){
-            p1++;
-            nums[p1] = nums[p2];
+    for (let p2 = 1; p2 < nums.length; p2++){   // for loop for pointer two starting at 2nd elem
+        if (nums[p1] !== nums[p2]){             // if p1 is the same as p2 move p1 
+            p1++;                               // make the elem after p1
+            nums[p1] = nums[p2];                // equal to p2
         }
     }
     
-    return p1 + 1;
+    return p1 + 1; // increment p1 by 1 to get the length of the non repeating elems
 };
 
 // 2 pointer solution
