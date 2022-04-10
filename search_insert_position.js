6df2d3abafc2c35e8ba1851530cc5faf3c1f58f8
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-09 22:25:25 -0400

    search insert position, working solution

--- search_insert_position.js
+++ search_insert_position.js
@@ -4,5 +4,11 @@
  * @return {number}
  */
  var searchInsert = function(nums, target) {
-    
+    for (let i=0; i<nums.length; i++){
+        if (nums[i] > target)  return i;
+        if (nums[i] === target) return i;
+        if (nums[i+1] > target || i+1 === nums.length){
+            return i+1;
+        }
+    }
 };
