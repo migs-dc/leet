78c7aac36a6decdfe910e6ec4f426c901524d803
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-10 23:11:02 -0400

    merge sorted array - different working solution, but not all cases

--- merge_sorted_array.js
+++ merge_sorted_array.js
@@ -6,22 +6,21 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
  var merge = function(nums1, m, nums2, n) {
-    // if (m === 0) return nums2;
-    // if (n === 0) return nums1;
+    if (m === 0) return nums2;
+    if (n === 0) return nums1;
     
-    let p = 0;
-    let temp;
-    
-    for (let i=0; i<m+n; i++){
-        if (nums1[i] > nums2[p]){
-            temp = nums1[i]
-            nums1[i] = nums2[p];
-            nums2[p] = temp;
-        } 
-        if ( nums1[i] === 0 ){
-            nums1[i] = nums2[p];
-            p++;
+    let p1 = m-1;
+    let p2 = n-1;
+    // consol.log(p1);
+    for (let i=nums1.length-1; i>=0; i--){
+        if (nums1[p1] > nums2[p2]){
+            nums1[i] = nums1[p1];
+            p1--;
+        } else {
+            nums1[i] = nums2[p2];
+            p2--;
         }
+        if (p2 < 0) break;
     }
     
     return nums1;
