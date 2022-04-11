dac9798ae9d027bfbedd7fd13378063b4c50e64b
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-10 23:14:12 -0400

    merge sorted array - done

--- merge_sorted_array.js
+++ merge_sorted_array.js
@@ -6,13 +6,13 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
  var merge = function(nums1, m, nums2, n) {
-    if (m === 0) return nums2;
-    if (n === 0) return nums1;
     
     let p1 = m-1;
     let p2 = n-1;
-    // consol.log(p1);
+    
     for (let i=nums1.length-1; i>=0; i--){
+        if (p2 < 0) break;
+        
         if (nums1[p1] > nums2[p2]){
             nums1[i] = nums1[p1];
             p1--;
@@ -20,7 +20,6 @@
             nums1[i] = nums2[p2];
             p2--;
         }
-        if (p2 < 0) break;
     }
     
     return nums1;
