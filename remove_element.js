b739fbde1eddf70a447312920dae02b8057f409d
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-12 22:26:03 -0400

    remove element - working answer

--- remove_element.js
+++ remove_element.js
@@ -4,5 +4,14 @@
  * @return {number}
  */
  var removeElement = function(nums, val) {
+    p1 = 0;
     
+    for (let i=0; i<nums.length; i++){
+        if (nums[i] !== val){
+            nums[p1] = nums[i];
+            p1++;
+        } 
+    }
+    
+    return p1;
 };
