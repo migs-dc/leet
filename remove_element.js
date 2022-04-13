754d032d07da45ccf010a3f8852da7f6629e10e3
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-12 22:31:01 -0400

    change i to p2 and add proper declaration to p1

--- remove_element.js
+++ remove_element.js
@@ -4,14 +4,14 @@
  * @return {number}
  */
  var removeElement = function(nums, val) {
-    p1 = 0;
+    let p1 = 0; // points to our return value
     
-    for (let i=0; i<nums.length; i++){
-        if (nums[i] !== val){
-            nums[p1] = nums[i];
-            p1++;
+    for (let p2=0; p2<nums.length; p2++){   // iterates through each elem our p2
+        if (nums[p2] !== val){              // if p2 is pointing at an elem that isn't val
+            nums[p1] = nums[p2];            // make p1 = p2
+            p1++;                           // move p1
         } 
     }
     
-    return p1;
-};
+    return p1;  //p1 will represent the length of the answer withing nums
+};  
