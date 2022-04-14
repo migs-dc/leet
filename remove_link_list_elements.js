50693aca335d59763a1fcf8dc9fedcd360493324
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-13 23:19:46 -0400

    remove link list elements, working answer

--- remove_link_list_elements.js
+++ remove_link_list_elements.js
@@ -11,5 +11,20 @@
  * @return {ListNode}
  */
  var removeElements = function(head, val) {
+    if (head === null) return null;
+    
+    let currentNode = new ListNode(-1);
+    currentNode.next = head;
+    head = currentNode;
+    
+    while (currentNode.next !== null) {
+        if (currentNode.next.val === val) {
+            currentNode.next = currentNode.next.next;
+        } else {
+             currentNode = currentNode.next;
+        }
+    }
+    
+    return head.next;
     
 };
