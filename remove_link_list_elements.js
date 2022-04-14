ab15ec62274ebc60a3a26ba8450588843584af3c
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-13 23:25:17 -0400

    remove link list elements, add comments

--- remove_link_list_elements.js
+++ remove_link_list_elements.js
@@ -10,21 +10,21 @@
  * @param {number} val
  * @return {ListNode}
  */
- var removeElements = function(head, val) {
-    if (head === null) return null;
+ var removeElements = function(head, val) { 
+    if (head === null) return null; // simply return null if LL is empty
     
-    let currentNode = new ListNode(-1);
-    currentNode.next = head;
-    head = currentNode;
+    let currentNode = new ListNode(-1); // dummy node
+    currentNode.next = head;            // add LL to dummy node
+    head = currentNode;                 // make dummy node head
     
-    while (currentNode.next !== null) {
-        if (currentNode.next.val === val) {
-            currentNode.next = currentNode.next.next;
+    while (currentNode.next !== null) {                 // iterate through LL until the end is found
+        if (currentNode.next.val === val) {             // if the next node's value = val
+            currentNode.next = currentNode.next.next;   // skip over it by assigning the current nodes next to the one                                                              after it hence the .next.next
         } else {
-             currentNode = currentNode.next;
+             currentNode = currentNode.next;            // otherwise just move on
         }
     }
     
-    return head.next;
+    return head.next;   // return head without the dummy node at the front
     
 };
