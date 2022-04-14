f4d30f36c200471651289aa90e4a33dc443b4e56
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-13 23:28:32 -0400

    remove zeroes, pseudocode

--- move_zeroes.js
+++ move_zeroes.js
@@ -3,5 +3,7 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
  var moveZeroes = function(nums) {
-    
+    // two pointers,
+    // one iterates through entire nums.
+    // p2 looks for non-zeros to swap to the 0s that p1 finds
 };
