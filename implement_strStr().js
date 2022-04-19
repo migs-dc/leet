ee343d3989f64d2cef027201fb333a0413c431d8
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-18 22:53:33 -0400

    implement strStr(), add comments

--- implement_strStr().js
+++ implement_strStr().js
@@ -4,20 +4,20 @@
  * @return {number}
  */
  var strStr = function(haystack, needle) {
-    var res = -1;
+    var res = -1;                           // result
     
-    for (let i=0; i<haystack.length; i++){
-        if (haystack[i] === needle[0]){
-            res = i;
-            for (let j=1; j<needle.length; j++){
+    for (let i=0; i<haystack.length; i++){  // iterate through haystack
+        if (haystack[i] === needle[0]){     // current elem in haystack is same as first elem in needle
+            res = i;                        // if it is change res to i
+            for (let j=1; j<needle.length; j++){    // iterate through next elem in needle and check the next elem in the haystack as well
                 if (haystack[i+j] !== needle[j]){
-                    res = -1;
+                    res = -1;                       // if they don't match just put res back to -1 and break
                     break;
                 }
             }
-            if (res !== -1) return res;
+            if (res !== -1) return res;             // if after the inner for loop the res is not -1 it means that the needle was found in the heystack
         }
     }
     
-    return res;
+    return res; // -1 will just be returned if needle is not found
 };
