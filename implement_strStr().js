05b12c36154f3a7361f934904d42a02e7e6b10b8
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-18 22:49:39 -0400

    implement strStr(), working solution

--- implement_strStr().js
+++ implement_strStr().js
@@ -4,5 +4,20 @@
  * @return {number}
  */
  var strStr = function(haystack, needle) {
+    var res = -1;
     
+    for (let i=0; i<haystack.length; i++){
+        if (haystack[i] === needle[0]){
+            res = i;
+            for (let j=1; j<needle.length; j++){
+                if (haystack[i+j] !== needle[j]){
+                    res = -1;
+                    break;
+                }
+            }
+            if (res !== -1) return res;
+        }
+    }
+    
+    return res;
 };
