9a26af438528202596d17546666c97dc9ee9dd4b
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 21:47:57 -0400

    length of last word - working solution, but quite inefficient

--- length_of_last_word.js
+++ length_of_last_word.js
@@ -7,5 +7,9 @@
     // we traverse the resulting array from behind
     // once we find somthing that isn't "" we return the length of that string
     
+    let arr = s.split(" ");
     
+    for (let i = arr.length-1; i >= 0; i--){
+        if (arr[i].length >= 1) return arr[i].length; 
+    }
 };
