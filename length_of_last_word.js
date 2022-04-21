a5ab36d48c42749cf6eee0507de558ee47bb9353
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 22:00:24 -0400

    length of last word - working solution, add 1 extra solution from a java user and comments + time and space complexity

--- length_of_last_word.js
+++ length_of_last_word.js
@@ -18,18 +18,40 @@
 //     // one liner
 //     return s.split(' ').filter((w) => w != '').reverse()[0].length
     
+    
     // efficient pointer solution
     let length = 0;
     
-    for (let i = s.length-1; i >= 0; i--){
-        if ( s[i] === ' ' ){
-            if ( length > 0){
-                return length;
+    for (let i = s.length-1; i >= 0; i--){  // traverse from back of string
+        if ( s[i] === ' ' ){                // if a space is found
+            if ( length > 0){               // check if a word has been found before. if length > 0
+                return length;              // just return length because the last word has been found
             }
         } else {
-            length++;
+            length++;                       // otherwise the word is being found letter by letter
         }
-    }
-    return length;
+    }       
+    return length;                          // just return the word causet there were no white spaces at the front of the string
     
-};
+
+    
+//     // apparently 100% faster
+//     let i = s.length - 1;
+//     let c = 0;
+//     while(i >= 0){
+//         if (s[i] != ' '){
+//             c++;
+//             if (i-1 >= 0 && s[i-1] == ' '){
+//                 break;
+//             }
+//         }
+//         i--;
+//     }
+//     return c;
+    
+//     // it wasn't
+    
+    
+    // time = O(n);
+    // space = 0(1);
+}
