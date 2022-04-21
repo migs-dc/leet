d31b44e5f05ed255b5de7f7badc25ff95d2203c1
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 21:54:00 -0400

    length of last word - working solution, add 2 new solutions. one one liner and another with just a for loop

--- length_of_last_word.js
+++ length_of_last_word.js
@@ -7,9 +7,29 @@
     // we traverse the resulting array from behind
     // once we find somthing that isn't "" we return the length of that string
     
-    let arr = s.split(" ");
     
-    for (let i = arr.length-1; i >= 0; i--){
-        if (arr[i].length >= 1) return arr[i].length; 
+//     // slow as heck solution
+//     let arr = s.split(" ");
+//     for (let i = arr.length-1; i >= 0; i--){
+//         if (arr[i].length >= 1) return arr[i].length; 
+//     }
+    
+    
+//     // one liner
+//     return s.split(' ').filter((w) => w != '').reverse()[0].length
+    
+    // efficient pointer solution
+    let length = 0;
+    
+    for (let i = s.length-1; i >= 0; i--){
+        if ( s[i] === ' ' ){
+            if ( length > 0){
+                return length;
+            }
+        } else {
+            length++;
+        }
     }
+    return length;
+    
 };
