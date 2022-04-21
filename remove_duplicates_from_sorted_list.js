017eac195bc2d1cab973646f550e8d2b5a905ff1
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 21:18:14 -0400

    remove duplicates from sorted list, add comments

--- remove_duplicates_from_sorted_list.js
+++ remove_duplicates_from_sorted_list.js
@@ -11,29 +11,16 @@
  */
  var deleteDuplicates = function(head) {
     
-    //     let currentNode = new ListNode(-1);
-    //     currentNode.next = head;
-    //     head = currentNode;
-        
-    //     let resNode = new ListNode(-1);
-    //     resNode.next = head.next;
-        
-    //     while(currentNode.next !== null){
-    //         if (currentNode !== currentNode.next) {
-    //             resNode.next = c
-    //         }
-    //     }
-        
-        let currentNode = head;
-        
-        while(currentNode && currentNode.next){
-            if (currentNode.val == currentNode.next.val){
-                currentNode.next = currentNode.next.next;
-            } else {
-                currentNode = currentNode.next;
-            }
-        }
-        
-        return head;
-        
-    };
+    let currentNode = head; // getting the ref of the head of the LL from head and putting it on our currentNode
+    
+    while(currentNode && currentNode.next){             // loop keeps going until neither currentNode or it's next node are null
+        if (currentNode.val == currentNode.next.val){   // if the val of the currentNode and the next one are the same
+            currentNode.next = currentNode.next.next;   // we simply make our current node point to the node after the current node
+        } else {                                        // we repeat this until our currentNode's val is no longer the same with the next node's val
+            currentNode = currentNode.next;             // then we just change the reference of currentNode. it's a little bit like reccursion in way that we're reusing currentNode.
+        }                                                           // * just had a little bit of trouble wrapping it around my head for a bit
+    }
+    
+    return head;                                        // after that we simply return our head and all it's pointing too now
+    
+};
