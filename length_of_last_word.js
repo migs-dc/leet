5d6af38579bd09ce9fbcba7220e575b786b5d7b4
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 21:35:09 -0400

    length of last word, pseudo code

--- length_of_last_word.js
+++ length_of_last_word.js
@@ -3,5 +3,9 @@
  * @return {number}
  */
  var lengthOfLastWord = function(s) {
+    // split s with " "
+    // we traverse the resulting array from behind
+    // once we find somthing that isn't "" we return the length of that string
+    
     
 };
