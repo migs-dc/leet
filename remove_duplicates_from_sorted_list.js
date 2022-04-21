9bb1adf8f5ed7fa0d4da3f2d5d18cb3ee8c50a79
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 21:09:45 -0400

    remove duplicates from sorted list, working answer

--- remove_duplicates_from_sorted_list.js
+++ remove_duplicates_from_sorted_list.js
@@ -11,4 +11,29 @@
  */
  var deleteDuplicates = function(head) {
     
-};
+    //     let currentNode = new ListNode(-1);
+    //     currentNode.next = head;
+    //     head = currentNode;
+        
+    //     let resNode = new ListNode(-1);
+    //     resNode.next = head.next;
+        
+    //     while(currentNode.next !== null){
+    //         if (currentNode !== currentNode.next) {
+    //             resNode.next = c
+    //         }
+    //     }
+        
+        let currentNode = head;
+        
+        while(currentNode && currentNode.next){
+            if (currentNode.val == currentNode.next.val){
+                currentNode.next = currentNode.next.next;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+        
+        return head;
+        
+    };
