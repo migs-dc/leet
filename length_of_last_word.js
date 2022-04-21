540afa58485501bfff366976441a92a99c69a79d
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-20 22:01:50 -0400

    length of last word - working solution, clean up

--- length_of_last_word.js
+++ length_of_last_word.js
@@ -6,6 +6,23 @@
     // split s with " "
     // we traverse the resulting array from behind
     // once we find somthing that isn't "" we return the length of that string
+
+        // efficient pointer solution
+        let length = 0;
+    
+        for (let i = s.length-1; i >= 0; i--){  // traverse from back of string
+            if ( s[i] === ' ' ){                // if a space is found
+                if ( length > 0){               // check if a word has been found before. if length > 0
+                    return length;              // just return length because the last word has been found
+                }
+            } else {
+                length++;                       // otherwise the word is being found letter by letter
+            }
+        }       
+        return length;                          // just return the word causet there were no white spaces at the front of the string    
+        
+        // time = O(n);
+        // space = 0(1);
     
     
 //     // slow as heck solution
@@ -17,23 +34,6 @@
     
 //     // one liner
 //     return s.split(' ').filter((w) => w != '').reverse()[0].length
-    
-    
-    // efficient pointer solution
-    let length = 0;
-    
-    for (let i = s.length-1; i >= 0; i--){  // traverse from back of string
-        if ( s[i] === ' ' ){                // if a space is found
-            if ( length > 0){               // check if a word has been found before. if length > 0
-                return length;              // just return length because the last word has been found
-            }
-        } else {
-            length++;                       // otherwise the word is being found letter by letter
-        }
-    }       
-    return length;                          // just return the word causet there were no white spaces at the front of the string
-    
-
     
 //     // apparently 100% faster
 //     let i = s.length - 1;
@@ -48,10 +48,5 @@
 //         i--;
 //     }
 //     return c;
-    
 //     // it wasn't
-    
-    
-    // time = O(n);
-    // space = 0(1);
 }
