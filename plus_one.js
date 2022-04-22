9f540edd4a72a2250712f901ab3d79553a602f60
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-21 23:33:52 -0400

    plus one - working solution

--- plus_one.js
+++ plus_one.js
@@ -3,5 +3,8 @@
  * @return {number[]}
  */
  var plusOne = function(digits) {
-    
+    for (let i = 0; i < digits.length; i++) {
+      let num = BigInt(digits.join('')) + 1n;
+      return Array.from(num.toString()).join().split(',');
+    }
 };
