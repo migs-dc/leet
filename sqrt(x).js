885a859e91e8fed17aee524b4f64f29d58d3f692
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-21 23:40:17 -0400

    sqrt(x) - cleaned code

--- sqrt(x).js
+++ sqrt(x).js
@@ -3,22 +3,21 @@
  * @param {string} b
  * @return {string}
  */
- const mySqrt = function (x) {
+const mySqrt = function (x) {
     let left = 0;
     let right = x;
-  
+
     while (left <= right) {
-      const mid = left + Math.floor((right - left) / 2);
+        const mid = left + Math.floor((right - left) / 2);
       const value = mid ** 2;
-  
-      if (value < x) {
-        left = mid + 1;
-      } else if (value > x) {
-        right = mid - 1;
-      } else {
-        return mid;
-      }
+
+        if (value < x) {
+            left = mid + 1;
+        } else if (value > x) {
+            right = mid - 1;
+        } else {
+            return mid;
+        }
     }
-  
     return right;
-  };
+};
