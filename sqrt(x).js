25c2f36ec95187ad7cd676083dcd6935f7d56cca
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-21 23:38:37 -0400

    sqrt(x) - working solution

--- sqrt(x).js
+++ sqrt(x).js
@@ -3,6 +3,22 @@
  * @param {string} b
  * @return {string}
  */
- var addBinary = function(a, b) {
-    
-};
+ const mySqrt = function (x) {
+    let left = 0;
+    let right = x;
+  
+    while (left <= right) {
+      const mid = left + Math.floor((right - left) / 2);
+      const value = mid ** 2;
+  
+      if (value < x) {
+        left = mid + 1;
+      } else if (value > x) {
+        right = mid - 1;
+      } else {
+        return mid;
+      }
+    }
+  
+    return right;
+  };
