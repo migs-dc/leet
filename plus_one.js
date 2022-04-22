9f932510fc1be9a9d49ee640ed2e416d1531417c
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-21 23:36:31 -0400

    plus one - another solution

--- plus_one.js
+++ plus_one.js
@@ -8,3 +8,23 @@
       return Array.from(num.toString()).join().split(',');
     }
 };
+
+var plusOne = function(digits) {
+    if(digits[digits.length - 1]<9){
+        digits[digits.length - 1] += 1;
+    } else {
+        for(let i=digits.length-1; i>=0; i--){
+            if(digits[i] === 9){
+                digits[i] = 0;
+            } else {
+                digits[i] = digits[i] + 1;
+                break;
+            }
+        }
+        if(digits[0] === 0){
+            digits.unshift(1);
+        }
+    }
+    
+    return digits;
+};
