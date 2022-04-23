71da9e6a5b756387c455b6834375adba72e9fff8
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-22 22:51:34 -0400

    climbing stairs - clean solution add comments

--- climbing_stairs.js
+++ climbing_stairs.js
@@ -2,7 +2,7 @@
  * @param {number} n
  * @return {number}
  */
- var climbStairs = function (n) {
+var climbStairs = function (n) {
     // iterative
     let arr = new Array(n + 1);
     arr.fill(-1);
@@ -10,11 +10,11 @@
     arr[1] = 1;
     arr[2] = 2;
     arr[3] = 3;
-  
+
     for (var i = 4; i <= n; i++) arr[i] = arr[i - 1] + arr[i - 2];
-  
+
     return arr[n];
-  
+
     // DP
     //   let arr = new Array(n + 1);
     //   arr.fill(-1);
@@ -22,12 +22,12 @@
     //   arr[1] = 1;
     //   arr[2] = 2;
     //   arr[3] = 3;
-  
+
     //   const helper = (n) => {
     //     if (arr[n] !== -1) return arr[n];
     //     arr[n] = helper(n - 1) + helper(n - 2);
     //     return arr[n];
     //   };
-  
+
     //   return helper(n);
-  };
+};
