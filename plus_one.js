59f0c763df61e50e90fcbcdbec94707b34176720
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-23 23:03:18 -0400

    plus one, cleaner solution

--- plus_one.js
+++ plus_one.js
@@ -2,10 +2,10 @@
  * @param {number[]} digits
  * @return {number[]}
  */
- var plusOne = function(digits) {
+var plusOne = function(digits) {
     for (let i = 0; i < digits.length; i++) {
-      let num = BigInt(digits.join('')) + 1n;
-      return Array.from(num.toString()).join().split(',');
+        let num = BigInt(digits.join('')) + 1n;
+        return Array.from(num.toString()).join().split(',');
     }
 };
 
