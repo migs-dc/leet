c9b8faa03ad9360b6b0676298ecb0feddb03da6d
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-27 23:41:13 -0400

    plus one, different solution

--- plus_one.js
+++ plus_one.js
@@ -1,30 +1,16 @@
-/**
- * @param {number[]} digits
- * @return {number[]}
- */
-var plusOne = function(digits) {
-    for (let i = 0; i < digits.length; i++) {
-        let num = BigInt(digits.join('')) + 1n;
-        return Array.from(num.toString()).join().split(',');
-    }
-};
-
-var plusOne = function(digits) {
-    if(digits[digits.length - 1]<9){
-        digits[digits.length - 1] += 1;
-    } else {
-        for(let i=digits.length-1; i>=0; i--){
-            if(digits[i] === 9){
+var plusOne = function(digits){
+    last = digits.length - 1;
+    if (digits[last] < 9) digits[last]++;
+    else {
+        for (let i = last; i >= 0; i--){
+            if (digits[i] === 9){
                 digits[i] = 0;
             } else {
-                digits[i] = digits[i] + 1;
+                digits[i]++;
                 break;
             }
         }
-        if(digits[0] === 0){
-            digits.unshift(1);
-        }
+        if (digits[0] === 0) digits.unshift(1);
     }
-    
     return digits;
 };
