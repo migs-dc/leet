aca646feed5dc59b4f02396af5eca7a308d208c0
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-04-29 23:26:13 -0400

    add binary, add comments

--- add_binary.js
+++ add_binary.js
@@ -5,22 +5,23 @@
  */
 
 var addBinary = function(a, b) {
-  let res = "";
-  let carry = 0;
+  let res = "";   // result
+  let carry = 0;  // carry after the last of each string
   
-  while (a || b || carry){
-      let sum = +a.slice(-1) + +b.slice(-1) + carry;
-      if (sum > 1){
-          res = sum % 2 + res;
+  while (a || b || carry){  // while none of these are undefined
+      let sum = +a.slice(-1) + +b.slice(-1) + carry;  // we just simply add them
+
+      if (sum > 1){ // if it's greater than on it means there's a carry
           carry = 1;
+          res = sum % 2 + res; // after that we just modulo to check what to put. if it's 2 -> 0 if 3 -> 1
       } else {
-          res  = sum + res;
           carry = 0;
+          res  = sum + res; // no carry so we just put either 1 or 0
       }
-      a = a.slice(0, -1);
+      a = a.slice(0, -1);   // remove the last element for a & b
       b = b.slice(0, -1);
   }
-  return res; 
+  return res; // return result
 };
 /* pseudo code:
 
