d0c16510396245ffc401639252d6292aba51a973
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-07 22:12:28 -0400

    same tree - add comments

--- easy/same_tree.js
+++ easy/same_tree.js
@@ -13,6 +13,18 @@
  */
  var isSameTree = function(p, q) {
     if (p === null || q === null) return p === q;
+        // check if either p and q are null and return whether they are both null
+
+    if (p.val !== q.val) return false; 
+        // if neither of them are null check if they have the same value
+        // return false if they don't
+    
+    return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+        // if they are the same it means we can go deper in the tree. starting with the right recursively
+};
+
+var isSameTree = function(p, q) {
+    if (p === null || q === null) return p === q;
     if (p.val !== q.val) return false;
     return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
 };
