ac5b703cd8895b95891bde8dbcbba1fc300ae23f
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-07 22:10:27 -0400

    same tree - working answer

--- easy/same_tree.js
+++ easy/same_tree.js
@@ -11,6 +11,8 @@
  * @param {TreeNode} q
  * @return {boolean}
  */
-var isSameTree = function(p, q) {
-
+ var isSameTree = function(p, q) {
+    if (p === null || q === null) return p === q;
+    if (p.val !== q.val) return false;
+    return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
 };
