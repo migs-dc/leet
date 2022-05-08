bbf52b0dbc6bebee03b19956c0d8af7fd3f09a94
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-07 22:28:37 -0400

    symmetric tree - working answer

--- easy/symmetric_tree.js
+++ easy/symmetric_tree.js
@@ -7,14 +7,6 @@
  * }
  */
 /**
- * Definition for a binary tree node.
- * function TreeNode(val, left, right) {
- *     this.val = (val===undefined ? 0 : val)
- *     this.left = (left===undefined ? null : left)
- *     this.right = (right===undefined ? null : right)
- * }
- */
-/**
  * @param {TreeNode} root
  * @return {boolean}
  */
@@ -27,5 +19,5 @@
 var isMirror = function(tree1, tree2){
     if (tree1 === null || tree2 === null) return tree1 === tree2;
     if (tree1.val !== tree2.val) return false;
-    return isMirror(tree1.left, tree2.right) && isMirror(tree2.right, tree1.left)
+    return isMirror(tree1.left, tree2.right) && isMirror(tree1.right, tree2.left)
 };
