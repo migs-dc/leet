cc42a4f3c706bdd9ec1a9a70385b7b88d2f493db
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-10 22:53:21 -0400

    symmetric tree, add comments

--- easy/symmetric_tree.js
+++ easy/symmetric_tree.js
@@ -11,13 +11,27 @@
  * @return {boolean}
  */
 var isSymmetric = function(root) {
-    if (root === null) return true;
+    if (root === null) return true; // edge case
     
-    return isMirror(root.left, root.right);
+    return isMirror(root.left, root.right); // since there is no need to check the root we just use a helper function
+        //and that function is what goes on recursively
 };
 
-var isMirror = function(tree1, tree2){
+var isMirror = function(tree1, tree2){ // helper function
+    if (tree1 === null || tree2 === null) return tree1 === tree2;   // check if either tree is null
+    if (tree1.val !== tree2.val) return false;                      // they are both not null check if they have the same value
+    return isMirror(tree1.left, tree2.right) && isMirror(tree1.right, tree2.left)   // if they both do we can check their children recursively by passing the correspoing pairs
+};
+
+
+var isSymmetric = function(root) {
+    if (root === null) return true; 
+    
+    return isMirror(root.left, root.right); 
+};
+
+var isMirror = function(tree1, tree2){ // helper function
     if (tree1 === null || tree2 === null) return tree1 === tree2;
     if (tree1.val !== tree2.val) return false;
     return isMirror(tree1.left, tree2.right) && isMirror(tree1.right, tree2.left)
-};`2`
+};
