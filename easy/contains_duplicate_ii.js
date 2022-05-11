7f864f0ca37e2a48de196efcaf634a5faaa57120
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-10 23:03:33 -0400

    contains duplicate

--- easy/contains_duplicate_ii.js
+++ easy/contains_duplicate_ii.js
@@ -4,15 +4,5 @@
  * @return {boolean}
 */
 var containsNearbyDuplicate = function(nums, k) {
-    let map = new Map()
 
-    for (let i = 0 ; i < nums.length ; i++){
-        let flag = map.has(nums[i])
-        let diffGreaterthanK = ((i -map.get(nums[i]) ) <=k)
-        
-        if(flag && diffGreaterthanK ) return true
-        else if (flag && !diffGreaterthanK ) map.set(nums[i],i)
-        else map.set(nums[i],i)
-    }
-    return false
 };
