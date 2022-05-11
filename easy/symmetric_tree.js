04910ca04e802934372b485fb8a28e7c7ca1b2fc
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-10 22:44:57 -0400

    cleaned up some code

--- easy/symmetric_tree.js
+++ easy/symmetric_tree.js
@@ -10,7 +10,7 @@
  * @param {TreeNode} root
  * @return {boolean}
  */
- var isSymmetric = function(root) {
+var isSymmetric = function(root) {
     if (root === null) return true;
     
     return isMirror(root.left, root.right);
@@ -20,4 +20,4 @@ var isMirror = function(tree1, tree2){
     if (tree1 === null || tree2 === null) return tree1 === tree2;
     if (tree1.val !== tree2.val) return false;
     return isMirror(tree1.left, tree2.right) && isMirror(tree1.right, tree2.left)
-};
+};`2`
