f90892c7c6af62476f97c8454b36f9a83c1501f6
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-11 23:31:02 -0400

    contains duplicate ii, add space and time

--- easy/contains_duplicate_ii.js
+++ easy/contains_duplicate_ii.js
@@ -4,21 +4,24 @@
  * @return {boolean}
  */
 var containsNearbyDuplicate = function(nums, k) {
-    let numsMap = new Map();
+    let numsMap = new Map(); // map for to keep track of the unique numbers we've encountered so far in the array
 
-    for (let i = 0; i < nums.length; i++){
-        let num = nums[i];
+    for (let i = 0; i < nums.length; i++){ // iterate through each elem in the array
+        let num = nums[i];  // just put nums[i] in a variable to make the code cleaner
 
-        if (numsMap.has(num) && i - numsMap.get(num) <= k) {
-            return true;
+        if (numsMap.has(num) && i - numsMap.get(num) <= k) { // check if num is already in the map (if it was encountered before) AND if the distance is <= k
+            return true; // if it is then return true
         } else {
-            numsMap.set(num, i);
+            numsMap.set(num, i); // if it's not then replace value of num in the map
         }
     }
 
-    return false;
+    return false; // if no duplicate was found in the right distance then false will just be returned 
+
 };
 
+// time: O(n log(n)) - because we just iterate through the array (n) and we use the binary tree search to search the map
+// space: 0(n) - we only store the unique numbers in the array so it's less than n (size of the array)
 
 
 var containsNearbyDuplicate = function(nums, k) {
