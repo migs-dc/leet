64db7789bf4146f18e6a0dfea3a5b1bcfd7cf246
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-11 23:28:48 -0400

    contains duplicate ii, add comments

--- easy/contains_duplicate_ii.js
+++ easy/contains_duplicate_ii.js
@@ -6,7 +6,25 @@
 var containsNearbyDuplicate = function(nums, k) {
     let numsMap = new Map();
 
-    for (let i = 0; i = nums.length; i++){
+    for (let i = 0; i < nums.length; i++){
+        let num = nums[i];
+
+        if (numsMap.has(num) && i - numsMap.get(num) <= k) {
+            return true;
+        } else {
+            numsMap.set(num, i);
+        }
+    }
+
+    return false;
+};
+
+
+
+var containsNearbyDuplicate = function(nums, k) {
+    let numsMap = new Map();
+
+    for (let i = 0; i < nums.length; i++){
         let num = nums[i];
 
         if (numsMap.has(num) && i - numsMap.get(num) <= k) {
