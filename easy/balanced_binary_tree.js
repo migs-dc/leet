df0091dc1892bb34c417108c8e6b9f1c8066ccf5
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-12 23:23:40 -0400

    balanced binary tree, initial answer

--- easy/balanced_binary_tree.js
+++ easy/balanced_binary_tree.js
@@ -26,3 +26,22 @@ let getHeight = function(node) {
 
     return Math.max(left, right) + 1;
 }
+
+//
+
+var isBalanced = function(root) {
+    if (root === null) return true;
+
+    return getHeight(root) !== -1;
+};
+
+let getHeight = function(node) {
+    if (node === null) return 0;
+
+    let left = getHeight(node.left);
+    let right = getHeight(node.right);
+
+    if (left === -1 || right === -1 || Math.abs(left - right) > 1) return -1;
+
+    return Math.max(left, right) + 1;
+}
