a05230e2a0547b3c5f0422efabdabe9c10af8a80
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-13 22:55:46 -0400

    minimum depth binary tree, comments

--- easy/minimum_depth_binary_tree.js
+++ easy/minimum_depth_binary_tree.js
@@ -11,8 +11,33 @@
  * @return {number}
  */
 var minDepth = function(root) {
-    if (root === null) return 0;
+    if (root === null) return 0;    // edge case
+    let queue = [];                 // initialize queue
+    queue.push(root);               // add root in queue
+    let depth = 0;                  // initialize variable for depth
 
+    while (queue.length !== 0){     // outer loop, as long as there are still children added to the queue that needs to be checked
+        let numberOfNodes = queue.length;   // make a variable for eh number of nodes in a level
+
+        while (numberOfNodes > 0){ // inner loop, as long as the queue is still not empty
+            let currentNode = queue.shift(); // top of the queue is removed and placed in currentNode
+            
+            if (currentNode.left === null && currentNode.right === null){ // if the currentNode has no children add to the depth count and simply return that because we found a leaf
+                depth++;
+                return depth;
+            } 
+            if (currentNode.left !== null) queue.push(currentNode.left);    // add children of the currentNode
+            if (currentNode.right !== null) queue.push(currentNode.right);
+            numberOfNodes--;    // after finishing checking that node and adding it's children decrement the numberOfNodes, which is a counter for how many nodes in that level
+        }
+        depth++;    // add one to depth after finishing a level (inner loop)
+    }
+};
+
+
+//no comments
+var minDepth = function(root) {
+    if (root === null) return 0;
     let queue = [];
     queue.push(root);
     let depth = 0;
@@ -29,7 +54,6 @@ var minDepth = function(root) {
             } 
             if (currentNode.left !== null) queue.push(currentNode.left);
             if (currentNode.right !== null) queue.push(currentNode.right);
-
             numberOfNodes--;
         }
         depth++;
