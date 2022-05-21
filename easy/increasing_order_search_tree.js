287ee12338e428a7e5ad858fcebfd4758441c133
Miguel Dela Cruz <delacruzcms@gmail.com>
2022-05-20 22:37:43 -0400

    longest substring without repeating characters

---
+++ easy/increasing_order_search_tree.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var increasingBST = function(root) {
+    let head = new TreeNode('dummy');   // initialize dummy head of new tree so that we can just return head.right later as our final result
+    let cursor = head;                  // cursor denotes current node in the new tree
+
+    inorder(root);                      // call helper function
+    return head.right;                  // return all but the dummy node
+
+    function inorder(node){             // helper function - where to recursion occurs
+        if(node === null) return;       // just return if node is null
+
+        inorder(node.left);             // first send the left of the node so that it's the first thing that gets added to the new tree
+
+        cursor.right = new TreeNode(node.val);      // add node to the right of the new tree 
+        cursor = cursor.right;                      // move cursor (pointer) to the newest node added
+
+        inorder(node.right);            // check the right node next
+    }
+};
+
+// time: O(n) - just travers n amount of nodes
+// space: O(n) - we make a new list of n nodes as well
+
+// code this again
